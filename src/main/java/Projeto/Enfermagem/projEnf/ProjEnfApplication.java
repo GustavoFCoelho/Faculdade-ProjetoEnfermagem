979d3552b68ecbbd9c4f9ffeef078c7a0c4d1011
GustavoFCoelho <gustavo.f.coelho09@gmail.com>
2019-11-07 13:53:12 -0300

    Commit qualquer

--- src/main/java/Projeto/Enfermagem/projEnf/ProjEnfApplication.java
+++ src/main/java/Projeto/Enfermagem/projEnf/ProjEnfApplication.java
@@ -15,42 +15,10 @@ import java.time.LocalTime;
 import java.util.Arrays;
 
 @SpringBootApplication
-public class ProjEnfApplication implements CommandLineRunner {
-
-	@Autowired
-	private PacInterface pacInterface;
-
-	@Autowired
-	private MedicamentoInterface medicamentoInterface;
+public class ProjEnfApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProjEnfApplication.class, args);
 	}
 
-	@Override
-	public void run(String... args) throws Exception {
-		Paciente pac = new Paciente();
-		pac.setPacnome("Gustavo");
-		pac.setPacsobrenome("Coelho");
-		pac.setPacalt("1.80");
-		pac.setPacpeso("120.00");
-		pac.setPacdtnasc(LocalDate.now());
-		pac.setPacdtentrada(LocalDate.now());
-		pac.setPacenc("Familia");
-		Paciente pac2 = new Paciente();
-		pac2.setPacnome("Frederson");
-		pac2.setPacsobrenome("Coelho");
-		pac2.setPacalt("1.80");
-		pac2.setPacpeso("120.00");
-		pac2.setPacdtnasc(LocalDate.now());
-		pac2.setPacdtentrada(LocalDate.now());
-		pac2.setPacenc("Familia");
-		pacInterface.saveAll(Arrays.asList(pac, pac2));
-		Medicamento medicamento = new Medicamento();
-		medicamento.setMedinfo("Clamnate");
-		medicamento.setMednome("Rivotril");
-		medicamento.setMeddosagem("15mg");
-		medicamentoInterface.save(medicamento);
-		System.out.println(LocalTime.now());
-	}
 }
